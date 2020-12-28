116d5520f1be25db59703ed4953970e8b092e395
will <alangprs1@gmail.com>
2020-12-28 15:32:08 +0800

    Initial Commit

---
+++ buySomthing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  buySomthing
+//
+//  Created by 翁燮羽 on 2020/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
